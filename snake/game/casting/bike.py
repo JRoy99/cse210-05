b870fcd7a8124dc84f6e10cb3d56557a72972a21
James Roy <no@email.com>
2022-10-15 09:10:20 -0600

    Snake game code

---
+++ snake/game/casting/bike.py
@@ -0,0 +1,68 @@
+import constants
+from game.casting.actor import Actor
+from game.shared.point import Point
+
+
+class Bike(Actor):
+    """
+    A zoomy light bike
+    
+    The responsibility of Bike is to move itself.
+
+    Attributes:
+        
+    """
+    def __init__(self):
+        super().__init__()
+        self._segments = []
+        self._prepare_body()
+
+    def get_segments(self):
+        return self._segments
+
+    def move_next(self):
+        # move all segments
+        for segment in self._segments:
+            segment.move_next()
+        # update velocities
+        for i in range(len(self._segments) - 1, 0, -1):
+            trailing = self._segments[i]
+            previous = self._segments[i - 1]
+            velocity = previous.get_velocity()
+            trailing.set_velocity(velocity)
+
+    def get_bike(self):
+        return self._segments[0]
+
+    def grow_trail(self, number_of_segments):
+        for i in range(number_of_segments):
+            trail = self._segments[-1]
+            velocity = trail.get_velocity()
+            offset = velocity.reverse()
+            position = trail.get_position().add(offset)
+            
+            segment = Actor()
+            segment.set_position(position)
+            segment.set_text("#")
+            segment.set_color(constants.GREEN)
+            self._segments.append(segment)
+
+    def turn_bike(self, velocity):
+        self._segments[0].set_velocity(velocity)
+    
+    def _prepare_body(self):
+        x = int(constants.MAX_X / 2)
+        y = int(constants.MAX_Y / 2)
+
+        for i in range(len(self._segments)+1):
+            position = Point(x - i * constants.CELL_SIZE, y)
+            velocity = Point(1 * constants.CELL_SIZE, 0)
+            text = "8" if i == 0 else "#"
+            color = constants.GREEN
+            
+            segment = Actor()
+            segment.set_position(position)
+            segment.set_velocity(velocity)
+            segment.set_text(text)
+            segment.set_color(color)
+            self._segments.append(segment)
